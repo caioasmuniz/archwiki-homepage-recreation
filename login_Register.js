8a6a3c25c00ebf7d2c5e3c11df5903efc43e8fa6
caioasmuniz <caiomuniz888@gmail.com>
2022-05-07 15:57:28 -0300

    reformatação

--- login_Register.js
+++ login_Register.js
@@ -1,46 +1,50 @@
-var email = document.getElementById('inputLog');
-var pw = document.getElementById('inputSenha');
+var email = document.getElementById("inputLog");
+var pw = document.getElementById("inputSenha");
 
-function showLogin(){    
-    var x = document.getElementById("loginPage");
+function showLogin() {
+  var x = document.getElementById("loginPage");
 
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
-function getLogin(){
-    axios.get('https://reqres.in/api/users/2')
-        .then(function(res){
-            var data = res.data.data;
-            console.log(res);
-            document.getElementById("inputLog").setAttribute('value', data.email);
-            document.getElementById("inputSenha").setAttribute('value', data.first_name + data.last_name);
-        });
-};
+function getLogin() {
+  axios.get("https://reqres.in/api/users/2").then(function (res) {
+    var data = res.data.data;
+    console.log(res);
+    document.getElementById("inputLog").setAttribute("value", data.email);
+    document
+      .getElementById("inputSenha")
+      .setAttribute("value", data.first_name + data.last_name);
+  });
+}
 
 function store() {
-    localStorage.setItem('email', email.value);
-    localStorage.setItem('pw', pw.value);
-    alert('Cadastro realizado');
+  localStorage.setItem("email", email.value);
+  localStorage.setItem("pw", pw.value);
+  alert("Cadastro realizado");
 }
 
-function processLogin(){
-    // valores registrados
-    var storedName = localStorage.getItem('email');
-    var storedPw = localStorage.getItem('pw');
+function processLogin() {
+  // valores registrados
+  var storedName = localStorage.getItem("email");
+  var storedPw = localStorage.getItem("pw");
 
-    // valores nos Inputs
-    var userName = document.getElementById('inputLog');
-    var userPw = document.getElementById('inputSenha');
+  // valores nos Inputs
+  var userName = document.getElementById("inputLog");
+  var userPw = document.getElementById("inputSenha");
 
-    if(userName.value == storedName && userPw.value == storedPw && userName.value != "" && userPw.value != "") {
-        alert('Você logou');
-    }else {
-        alert('nao logado');
-    }
+  if (
+    userName.value == storedName &&
+    userPw.value == storedPw &&
+    userName.value != "" &&
+    userPw.value != ""
+  ) {
+    alert("Você logou");
+  } else {
+    alert("nao logado");
+  }
 }
-
-
