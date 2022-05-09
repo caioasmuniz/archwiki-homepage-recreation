1f9d9c88eef6be291f000d30289e53dd4728a208
Lucas Paskua <80064601+paskua4@users.noreply.github.com>
2022-05-08 21:48:07 -0300

    exlusão de código nao usado

--- login_Register.js
+++ login_Register.js
@@ -1,7 +1,6 @@
 var email = document.getElementById("inputLog");
 var pw = document.getElementById("inputSenha");
 var loginPage = document.getElementById("loginPage");
-const cep = document.querySelector("#cep");
 
 function showLogin() {
   loginPage.style.display =
