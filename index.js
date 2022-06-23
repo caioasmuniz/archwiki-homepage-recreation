5d3d345719a572d8f21bb9d7776f252de91658cf
Lucas Paskua <80064601+paskua4@users.noreply.github.com>
2022-06-22 22:15:26 -0300

    Configurado para abrir a página html ao iniciar o servidor, utilizando handlebars (.hbs)

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ client.connect((err) => {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+app.use('/public', express.static(__dirname + '/public'))
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
