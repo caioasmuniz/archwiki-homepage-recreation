ba4a0a595279a8560c49d3c70c89f05bd493e179
Lucas Paskua <80064601+paskua4@users.noreply.github.com>
2022-06-26 19:39:34 -0300

    Implementaçao da validação do cadastro

--- model/Post.js
+++ model/Post.js
@@ -6,19 +6,44 @@ module.exports = class Post{
     static hash(string) {
         return createHash('sha256').update(string).digest('hex');
       }
+
+    static ValidateUserCadastro(email, senha, user) {
+      let error = false;
+      const re =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      if (!email.match(re) || email.length < 3) {
+        document.getElementById("invalidEmailCad").style.display = "block";
+        error = true;
+      }
+      if (senha.length < 3) {
+        document.getElementById("invalidPwCad").style.display = "block";
+        error = true;
+      }
+      if(user == null){
+        document.getElementById("invalidUserCad").style.display = "block";
+        error = true;
+      }
+    
+      return error;
+    }
+      
     
     static insert(data){
 
-        return client.connect("mongodb+srv://admin:n6vxXNG4oC9LI60P@cluster-trabalho-web.pzrykdc.mongodb.net/?retryWrites=true&w=majority", {
-            useNewUrlParser: true,                
-        }).then((client) => {
-            let db = client.db('projeto3');
-            return db.collection('users').insertOne({                
-                username: data.username,
-                email: data.email,
-                senha: this.hash(data.senha),                
-                publicacoes: 0
-            });
-        })        
+        let error = this.ValidateUserCadastro(data.email, data.senha, data.username);
+
+        if(!error){
+            return client.connect("mongodb+srv://admin:n6vxXNG4oC9LI60P@cluster-trabalho-web.pzrykdc.mongodb.net/?retryWrites=true&w=majority", {
+                useNewUrlParser: true,                
+            }).then((client) => {
+                let db = client.db('projeto3');
+                return db.collection('users').insertOne({                
+                    username: data.username,
+                    email: data.email,
+                    senha: this.hash(data.senha),                
+                    publicacoes: 0
+                });
+            })
+        }
     }
 }
